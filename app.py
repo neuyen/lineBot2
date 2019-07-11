a2d1e8355dae9b329ff22305c82f10629c6b1c0a
mon20979 <51431314+mon20979@users.noreply.github.com>
2019-07-11 10:09:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if (userText == 'ไอ้บ้า') :
+        sendText(user,('ไอ้โง่')
+    elif (userText == 'ไอ้ทุย') :
+        sendText(user,('มอๆๆ')
+    else :
+        sendText(user,('งงอ่ะ พูดใหม่ดิ
     return '',200
 
 def sendText(user, text):
